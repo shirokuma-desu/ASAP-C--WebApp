d51ba1f48b310d39891b42fa27c045abba843e88
shirokuma-desu <ezorcist.dt@gmail.com>
2022-07-19 20:33:43 +0700

    Add model

---
+++ FinalProject/Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace FinalProject.Models
+{
+    public partial class Movie
+    {
+        public Movie()
+        {
+            Rates = new HashSet<Rate>();
+        }
+
+        public int MovieId { get; set; }
+        public string Title { get; set; }
+        public int? Year { get; set; }
+        public int? GenreId { get; set; }
+        public string Image { get; set; }
+
+        public virtual Genre Genre { get; set; }
+        public virtual ICollection<Rate> Rates { get; set; }
+    }
+}
